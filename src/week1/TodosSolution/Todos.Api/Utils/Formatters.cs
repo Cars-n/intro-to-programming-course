95e0bab80cf43f981b1afc7de46c1831b32d4b67
Cars-n <carsonsmith8@hotmail.com>
2025-02-11 12:34:29 -0500

    Add unit test

---
+++ src/week1/TodosSolution/Todos.Api/Utils/Formatters.cs
@@ -0,0 +1,10 @@
+﻿namespace Todos.Api.Utils;
+
+public class Formatters
+{
+    // Method that takes two stringss and returns a string
+    public static string FormatName(string firstName, string lastName)
+    {
+        return $"{lastName}, {firstName}";
+    }
+}
